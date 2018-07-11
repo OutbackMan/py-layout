0414a00e8fe0419843d08765eb06ff04db7b8819
Ryan Edward <32256825+OutbackMan@users.noreply.github.com>
2018-07-11 11:03:39 +1000

    Update test.py

--- test.py
+++ test.py
@@ -1,7 +1,23 @@
-import sys
-
-reveal_type(sys.stdout)
 # consider implementing Vim autosave feature
+
+'''
+PILLOW:
+'''
+'''
+PYAUTOGUI:
+linux --> scrot
+'''
+
+# could optimise via c binding
+import pyautogui
+
+desktop_screenshot = pyautogui.screenshot()
+pyautogui.keyDown("shift")
+pyautogui.press("left")
+pyautogui.press("left")
+pyautogui.keyUp("shift")
+pyautogui.typewrite("text here")
+
 
 # moment is a weighted average of the pixels, giving a rough description of the shape
 # radius will be tweaked
@@ -11,5 +27,5 @@ def zernike_moment(image, polynomial_radius):
 # requires mahotas-imread
 img = mahotas.imread("image.jpeg", as_grey=True)
 numpy_img = img.astype(numpy.uint8)
-otsu_threshold = mahotas.otsu(numpy_img)
+otsu_threshold = mahotas.thresholding.otsu(numpy_img)
 binary_img = (numpy_img > otsu_threshold)
