8b994e7db14011fae56e8188283a797589a70c38
Russell Coight <rj.coight@gmail.com>
2018-07-14 15:02:08 +1000

    more restructuring

--- setup.py
+++ setup.py
@@ -4,6 +4,19 @@ import setuptools
 
 import typing
 import os
+import sys
+
+sys.path.insert(0, "src/")
+import config as ITEST_Config
+
+class CMakeExtension(setuptools.Extension):
+    def __init__(self, name, sourcedir):
+        super().__init__(self, name, sources=[])
+        self.sourcedir = os.path.abspath(sourcedir)
+
+class CMakeBuild(setuptools.command.build_ext):
+
+
 
 
 def _get_file_contents(file_name: str) -> str:
@@ -24,7 +37,21 @@ def _get_file_contents(file_name: str) -> str:
 
 
 setuptools.setup(
-	long_description=_get_file_contents("README")	
+        name=ITEST_Config.meta.name,
+        version=ITEST_Config.meta.version,
+        author=ITEST_Config.meta.author,
+        author_email=ITEST_Config.meta.author_email,
+        description=ITEST_Config.meta.description,
+        license=ITEST_Config.meta.license,
+        keywords=ITEST_Config.meta.keywords,
+        url=,
+        packages=,
+        platforms=,
+	long_description=_get_file_contents("README"),
+        classifiers=[
+            "Programming Language :: Python :: 3",
+            "Programming Language :: C",
+        ]
 )
 
 
